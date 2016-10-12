5788b67639af3e9498749b81ba4cd341ad67171d
Cuong Vu <cuongvu0103@gmail.com>
2016-10-12 16:25:28 +0700

    Initialize codebase

---
+++ spec/spec_helper.rb
@@ -0,0 +1,7 @@
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+Dir[File.dirname(__FILE__) + "/support/**/*.rb"].each { |file| require file }
+
+require "byebug"
+require "dotenv"
+require "gemini"
+Dotenv.load

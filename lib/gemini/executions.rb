0b72e4a31742d2edd1e12ab78e3fc78b3e4c88c2
Cuong Vu <cuongvu0103@gmail.com>
2016-11-22 18:02:32 +0700

    Add endpoint: get trades

--- lib/gemini/executions.rb
+++ lib/gemini/executions.rb
@@ -1,4 +1,10 @@
 module Gemini
   module Executions
+    def self.me(options = {})
+      executions = Gemini::Net.get("/v1/mytrades", options, true)
+      JSON.parse(executions).map do |execution|
+        Gemini::Execution.new(execution)
+      end
+    end
   end
 end
--- lib/gemini/model.rb
+++ lib/gemini/model.rb
@@ -7,6 +7,25 @@ module Gemini
 
   class Execution
     include Virtus.value_object
+
+    values do
+      attribute :tid, Integer
+      attribute :price, Decimal
+      attribute :amount, Decimal
+      attribute :timestamp, Integer
+      attribute :timestampms, Float
+      attribute :type, String
+      attribute :aggressor, Boolean
+      attribute :fee_currency, String
+      attribute :fee_amount, Decimal
+      attribute :order_id, String
+      attribute :client_order_id, String
+      attribute :break, String
+    end
+
+    def id
+      self.tid
+    end
   end
 
   class Balance

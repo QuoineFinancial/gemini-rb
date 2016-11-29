4d43cb0a036a14c23a1d8b00d0084fed4527c791
Cuong Vu <cuongvu0103@gmail.com>
2016-11-29 11:48:46 +0700

    Use POST instead of GET

--- lib/gemini/executions.rb
+++ lib/gemini/executions.rb
@@ -2,7 +2,7 @@ module Gemini
   module Executions
     def self.me(options = {})
       Gemini.sanity_check!
-      executions = Gemini::Net.get("/v1/mytrades", options, true)
+      executions = Gemini::Net.post("/v1/mytrades", options)
       JSON.parse(executions).map do |execution|
         Gemini::Execution.new(execution)
       end

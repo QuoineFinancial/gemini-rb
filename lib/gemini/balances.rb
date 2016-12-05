2a96aa8233db675b193a1b71f0a3331906c92016
Cuong Vu <cuongvu0103@gmail.com>
2016-12-05 11:29:09 +0700

    Replace GET by POST

--- lib/gemini/balances.rb
+++ lib/gemini/balances.rb
@@ -2,7 +2,7 @@ module Gemini
   module Balances
     def self.all(options = {})
       Gemini.sanity_check!
-      balances = Gemini::Net.get("/v1/balances", options, true)
+      balances = Gemini::Net.post("/v1/balances", options, true)
       JSON.parse(balances).map do |balance|
         Gemini::Balance.new(balance)
       end

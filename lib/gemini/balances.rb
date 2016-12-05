617f948d36c09a0145c25284841eaf647647d4df
Cuong Vu <cuongvu0103@gmail.com>
2016-12-05 11:36:09 +0700

    Fix number of arguments

--- lib/gemini/balances.rb
+++ lib/gemini/balances.rb
@@ -2,7 +2,7 @@ module Gemini
   module Balances
     def self.all(options = {})
       Gemini.sanity_check!
-      balances = Gemini::Net.post("/v1/balances", options, true)
+      balances = Gemini::Net.post("/v1/balances", options)
       JSON.parse(balances).map do |balance|
         Gemini::Balance.new(balance)
       end
